63d3ae11ce2f1d75cd847403adeb1120487a62d3
SrKomodo <komodosr@gmail.com>
2017-12-08 19:07:34 -0300

    Filter schedules that arent running

--- src/renderer/components/app/app.tsx
+++ src/renderer/components/app/app.tsx
@@ -7,7 +7,16 @@ interface Props {
 }
 
 function App(props: Props) {
-  const schedules = props.schedules.map(schedule =>
+  const schedules = props.schedules.filter(schedule => {
+    const now = new Date();
+    if (schedule.days.indexOf(now.getDay()) == -1) return false;
+    if (
+      schedule.starts.h * 60 + schedule.starts.m > now.getHours() * 60 + now.getMinutes() ||
+      schedule.ends.h * 60 + schedule.ends.m < now.getHours() * 60 + now.getMinutes()
+    ) return false;
+    return true;
+  })
+  .map(schedule =>
     <Schedule {...schedule} key={schedule.name}/>
   );
   return (
